72be053101bd015a6a86d5b53433ea50ee8bb362
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-20 11:52:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include 'includes/base_page/head_tag.php'
 <h1>New System Hello</h1>
 <h1>Awesomeness</h1>
 <h1>Awesomeness</h1>
+<h1>Great</h1>
 
 <?php
 include 'includes/base_page/footer_content.php'
