5b69b492c0e17043ce27048bf73ab1b45aea00fe
Timothy Karani <kartimothy@gmail.com>
2021-02-20 17:55:02 +0300

    A bit of styling done

--- banks/add_bank.php
+++ banks/add_bank.php
@@ -14,28 +14,28 @@ include "../includes/base_page/shared_top_tags.php"
       <div class="column">
         <label for="bank_name" class="label">Name</label>
         <div class="control">
-          <input type="text" name="bank_name" id="bank_name" class="input" required placeholder="Bank name">
+          <input type="text" name="bank_name" id="bank_name" class="input is-link" required placeholder="Bank name">
         </div>
       </div>
 
       <div class="column">
         <label for="branch_name" class="label">Branch</label>
         <div class="control">
-          <input type="text" name="branch_name" id="branch_name" class="input" required placeholder="Branch name">
+          <input type="text" name="branch_name" id="branch_name" class="input is-link" required placeholder="Branch name">
         </div>
       </div>
 
       <div class="column">
         <label for="account_number" class="label">Account Number</label>
         <div class="control">
-          <input type="number" name="account_number" id="account_number" class="input" required placeholder="Account number">
+          <input type="number" name="account_number" id="account_number" class="input is-link" required placeholder="Account number">
         </div>
       </div>
 
       <div class="column">
         <label for="account_number" class="label">Account Name</label>
         <div class="control commify">
-          <input type="text" name="account_name" id="account_name" class="input" required placeholder="Account name">
+          <input type="text" name="account_name" id="account_name" class="input is-link" required placeholder="Account name">
         </div>
       </div>
 
@@ -46,7 +46,7 @@ include "../includes/base_page/shared_top_tags.php"
       <div class="column commify">
         <label for="opening_balance" class="label">Opening Balance</label>
         <div class="control">
-          <input type="number" name="opening_balance" id="opening_balance" class="input" required placeholder="Opening balance">
+          <input type="number" name="opening_balance" id="opening_balance" class="input is-link" required placeholder="Opening balance">
         </div>
       </div>
 
@@ -64,7 +64,7 @@ include "../includes/base_page/shared_top_tags.php"
       <div class="column">
         <label for="cheque_clear_days" class="label">Cheque Clear Days</label>
         <div class="control">
-          <input type="number" name="cheque_clear_days" id="cheque_clear_days" class="input" required placeholder="Cheque clear days" max="15">
+          <input type="number" name="cheque_clear_days" id="cheque_clear_days" class="input is-link" required placeholder="Cheque clear days" max="15">
         </div>
       </div>
 
@@ -75,28 +75,28 @@ include "../includes/base_page/shared_top_tags.php"
       <div class="column">
         <label for="overdraft_interest" class="label">Overdraft Interest</label>
         <div class="control">
-          <input type="number" name="overdraft_interest" id="overdraft_interest" class="input" required placeholder="Overdraft interest" max="100">
+          <input type="number" name="overdraft_interest" id="overdraft_interest" class="input is-link" required placeholder="Overdraft interest" max="100">
         </div>
       </div>
 
       <div class="column">
         <label for="overlimit_interest" class="label">Overdraft Limit Interest</label>
         <div class="control">
-          <input type="number" name="overlimit_interest" id="overlimit_interest" class="input" required placeholder="Overdraft Limit interest" max="100">
+          <input type="number" name="overlimit_interest" id="overlimit_interest" class="input is-link" required placeholder="Overdraft Limit interest" max="100">
         </div>
       </div>
 
       <div class="column">
         <label for="late_payment_charges" class="label">Late Payment Charges</label>
         <div class="control">
-          <input type="number" name="late_payment_charges" id="late_payment_charges" class="input" required placeholder="Late payment charges" max="100">
+          <input type="number" name="late_payment_charges" id="late_payment_charges" class="input is-link" required placeholder="Late payment charges" max="100">
         </div>
       </div>
 
       <div class="column">
         <label for="overdraft_limit" class="label">Overdraft Limit</label>
         <div class="control">
-          <input type="number" name="overdraft_limit" id="overdraft_limit" class="input" required placeholder="Overdraft charges">
+          <input type="number" name="overdraft_limit" id="overdraft_limit" class="input is-link" required placeholder="Overdraft charges">
         </div>
       </div>
 
