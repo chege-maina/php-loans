fe1c4f9ef6ff2bd743a6cb68fa13f75f3f319ec6
Timothy Karani <kartimothy@gmail.com>
2021-02-20 21:20:20 +0300

    Sending data to server

--- payments/make_payment.php
+++ payments/make_payment.php
@@ -5,7 +5,7 @@ include "../includes/base_page/shared_top_tags.php"
 <div class="block title">
   Make Payment
 </div>
-<form action="#" method="post">
+<form onsubmit="return submitForm();">
   <div class="card">
 
     <div class="card-content">
@@ -15,18 +15,16 @@ include "../includes/base_page/shared_top_tags.php"
           <label for="supplier" class="label">Select Supplier</label>
           <div class="select is-fullwidth  ">
             <div class="control">
-              <select>
-                <option value="#">-- Select Supplier --</option>
+              <select id="supplier" required>
               </select>
             </div>
           </div>
         </div>
         <div class="column">
           <label for="bank" class="label">Select Bank</label>
-          <div class="select is-fullwidth">
+          <div class="select is-fullwidth required">
             <div class="control">
-              <select>
-                <option value="#">-- Select Bank --</option>
+              <select id="bank" required>
               </select>
             </div>
           </div>
@@ -35,7 +33,7 @@ include "../includes/base_page/shared_top_tags.php"
           <label for="date" class="label">Select Payment Date</label>
           <!-- autofill current date  -->
           <div class="control">
-            <input type="date" value="<?php echo date("Y-m-d"); ?>" id="date" class="input is-link">
+            <input type="date" value="<?php echo date("Y-m-d"); ?>" id="p_date" class="input is-link" required>
           </div>
         </div>
       </div>
@@ -45,22 +43,21 @@ include "../includes/base_page/shared_top_tags.php"
         <div class="column">
           <label for="cheque_number" class="label">Enter Cheque Number</label>
           <div class="control">
-            <input type="number" name="cheque_number" id="cheque_number" class="input" required placeholder="Cheque Number">
+            <input type="number" name="cheque_number" id="cheque_number" class="input" required placeholder="Cheque Number" required>
           </div>
         </div>
 
         <div class="column">
           <label for="amount_paid" class="label">Amount Paid</label>
           <div class="control">
-            <input type="number" name="amount_paid" id="amount_paid" class="input" required placeholder="Amount Paid">
+            <input type="number" name="amount_paid" id="amount_paid" class="input" required placeholder="Amount Paid" required>
           </div>
         </div>
 
         <div class="column">
           <label for="payment_type" class="label">Select Cheque Type</label>
-          <div class="select is-fullwidth">
-            <select>
-              <option value="#">-- Select Cheque Type --</option>
+          <div class="select is-fullwidth" required>
+            <select id="cheque_type" required>
             </select>
           </div>
         </div>
@@ -73,7 +70,69 @@ include "../includes/base_page/shared_top_tags.php"
     </div>
   </div>
 </form>
-</div>
+
+<script>
+  const supplier = document.querySelector("#supplier");
+  const bank = document.querySelector("#bank");
+  const p_date = document.querySelector("#p_date");
+  const cheque_number = document.querySelector("#cheque_number");
+  const amount_paid = document.querySelector("#amount_paid");
+  const change_type = document.querySelector("#change_type");
+
+  function submitForm() {
+
+    console.log("submitting");
+
+
+    const formData = new FormData();
+    formData.append("supplier_name", supplier.value);
+    formData.append("bank_name", bank.value);
+    formData.append("date", p_date.value);
+    formData.append("cheque_type", cheque_type.value);
+    formData.append("cheque_no", cheque_number.value);
+    formData.append("amount", amount_paid.value);
+
+    fetch('../includes/add_payment.php', {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => response.json())
+      .then(result => {
+        console.log('Success:', result);
+        if (result["message"] === "success") {
+          showSuccessAlert("Record stored successfuly");
+          reloadPage();
+        } else {
+          let msg = !("desc" in result) || result["desc"].trim() === "" ?
+            "Record not stored" : result["desc"];
+          showDangerAlert(msg);
+          removeAlert();
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        showDangerAlert("Could not send data to server");
+        removeAlert();
+      });
+
+    return false;
+  }
+
+  window.addEventListener('DOMContentLoaded', (event) => {
+    initSelectElement("#supplier", "-- Select Supplier --");
+    populateSelectElement("#supplier", "../includes/load_supplier.php", "name");
+
+
+    initSelectElement("#bank", "-- Select Bank --");
+    populateSelectElement("#bank", "../includes/load_bank.php", "name");
+
+
+    // TODO: Fetch from the right table
+    initSelectElement("#cheque_type", "-- Select Cheque Type --");
+    populateSelectElement("#cheque_type", "../includes/load_currency.php", "name");
+  });
+</script>
+
 <?php
 include "../includes/base_page/shared_bottom_tags.php"
 ?>
