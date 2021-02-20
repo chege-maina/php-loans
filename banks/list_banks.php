6f710dd660a6786bd90d1dbc7f784f0dac92ed02
Timothy Karani <kartimothy@gmail.com>
2021-02-20 15:55:07 +0300

    Added listing table

---
+++ banks/list_banks.php
@@ -0,0 +1,167 @@
+<?php
+include "../includes/base_page/shared_top_tags.php"
+?>
+
+<div class="block title">List of Banks</div>
+
+<div class="box">
+
+
+  <table id="table_tag" class="table is-striped is-hoverable is-fullwidth">
+    <thead>
+      <tr>
+        <th>Header 1</th>
+        <th>Header 2</th>
+        <th>Header 3</th>
+        <th>Header 4</th>
+        <th>Header 5</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Row:1 Cell:1</td>
+        <td>Row:1 Cell:2</td>
+        <td>Row:1 Cell:3</td>
+        <td>Row:1 Cell:4</td>
+        <td>Row:1 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:2 Cell:1</td>
+        <td>Row:2 Cell:2</td>
+        <td>Row:2 Cell:3</td>
+        <td>Row:2 Cell:4</td>
+        <td>Row:2 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:3 Cell:1</td>
+        <td>Row:3 Cell:2</td>
+        <td>Row:3 Cell:3</td>
+        <td>Row:3 Cell:4</td>
+        <td>Row:3 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:4 Cell:1</td>
+        <td>Row:4 Cell:2</td>
+        <td>Row:4 Cell:3</td>
+        <td>Row:4 Cell:4</td>
+        <td>Row:4 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:5 Cell:1</td>
+        <td>Row:5 Cell:2</td>
+        <td>Row:5 Cell:3</td>
+        <td>Row:5 Cell:4</td>
+        <td>Row:5 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:6 Cell:1</td>
+        <td>Row:6 Cell:2</td>
+        <td>Row:6 Cell:3</td>
+        <td>Row:6 Cell:4</td>
+        <td>Row:6 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:7 Cell:1</td>
+        <td>Row:7 Cell:2</td>
+        <td>Row:7 Cell:3</td>
+        <td>Row:7 Cell:4</td>
+        <td>Row:7 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:8 Cell:1</td>
+        <td>Row:8 Cell:2</td>
+        <td>Row:8 Cell:3</td>
+        <td>Row:8 Cell:4</td>
+        <td>Row:8 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:9 Cell:1</td>
+        <td>Row:9 Cell:2</td>
+        <td>Row:9 Cell:3</td>
+        <td>Row:9 Cell:4</td>
+        <td>Row:9 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:10 Cell:1</td>
+        <td>Row:10 Cell:2</td>
+        <td>Row:10 Cell:3</td>
+        <td>Row:10 Cell:4</td>
+        <td>Row:10 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:11 Cell:1</td>
+        <td>Row:11 Cell:2</td>
+        <td>Row:11 Cell:3</td>
+        <td>Row:11 Cell:4</td>
+        <td>Row:11 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:12 Cell:1</td>
+        <td>Row:12 Cell:2</td>
+        <td>Row:12 Cell:3</td>
+        <td>Row:12 Cell:4</td>
+        <td>Row:12 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:13 Cell:1</td>
+        <td>Row:13 Cell:2</td>
+        <td>Row:13 Cell:3</td>
+        <td>Row:13 Cell:4</td>
+        <td>Row:13 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:14 Cell:1</td>
+        <td>Row:14 Cell:2</td>
+        <td>Row:14 Cell:3</td>
+        <td>Row:14 Cell:4</td>
+        <td>Row:14 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:15 Cell:1</td>
+        <td>Row:15 Cell:2</td>
+        <td>Row:15 Cell:3</td>
+        <td>Row:15 Cell:4</td>
+        <td>Row:15 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:16 Cell:1</td>
+        <td>Row:16 Cell:2</td>
+        <td>Row:16 Cell:3</td>
+        <td>Row:16 Cell:4</td>
+        <td>Row:16 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:17 Cell:1</td>
+        <td>Row:17 Cell:2</td>
+        <td>Row:17 Cell:3</td>
+        <td>Row:17 Cell:4</td>
+        <td>Row:17 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:18 Cell:1</td>
+        <td>Row:18 Cell:2</td>
+        <td>Row:18 Cell:3</td>
+        <td>Row:18 Cell:4</td>
+        <td>Row:18 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:19 Cell:1</td>
+        <td>Row:19 Cell:2</td>
+        <td>Row:19 Cell:3</td>
+        <td>Row:19 Cell:4</td>
+        <td>Row:19 Cell:5</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <script>
+    window.addEventListener('DOMContentLoaded', (event) => {
+      $('#table_tag').DataTable();
+    });
+  </script>
+
+</div>
+
+<?php
+include "../includes/base_page/shared_bottom_tags.php"
+?>
