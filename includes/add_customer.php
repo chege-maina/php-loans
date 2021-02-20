6ac7ec610b3aa10804cd1ffbc59a27af1a47bb29
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-20 17:47:24 +0300

    Create add_customer.php

---
+++ includes/add_customer.php
@@ -0,0 +1,69 @@
+<?php
+include_once '../includes/dbconnect.php';
+$con = $conn;
+if (mysqli_connect_errno()) {
+  // If there is an error with the connection, stop the script and display the error.
+  die('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+function sanitize_input($data)
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = sanitize_input($_POST["name"]);
+  $email = sanitize_input($_POST["email"]);
+  $tel_no = sanitize_input($_POST["tel_no"]);
+  $postal_address = sanitize_input($_POST["postal_address"]);
+  $physical_address = sanitize_input($_POST["physical_address"]);
+
+  if ($stmt = $con->prepare('SELECT name FROM tbl_customer WHERE name = ? and email =?')) {
+    // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+    $stmt->bind_param('ss', $name, $email);
+    $stmt->execute();
+    // Store the result so we can check if the account exists in the database.
+    $stmt->store_result();
+    if ($stmt->num_rows == 0) {
+      if ($stmt = $con->prepare('INSERT INTO tbl_customer (name, email, tel_no, 
+      postal_address, physical_address) VALUES (?,?,?,?,?)')) {
+        $stmt->bind_param('sssss', $name, $email, $tel_no, $postal_address, $physical_address);
+
+        if ($stmt->execute()) {
+          $responseArray = array(
+            "message" => "success"
+          );
+        } else {
+          $responseArray = array(
+            "message" => "error",
+            "desc" => "Internal Server Error"
+          );
+        }
+        echo json_encode($responseArray);
+      } else {
+        echo json_encode(array(
+          "message" => "error",
+          "desc" => mysqli_error($con)
+        ));
+      }
+    } else {
+      echo json_encode(array(
+        "message" => "error",
+        "desc" => "Supplier Already exists.."
+      ));
+    }
+  } else {
+    echo json_encode(array(
+      "message" => "error",
+      "desc" => "Database Connection Error.."
+    ));
+  }
+} else {
+  echo json_encode(array(
+    "message" => "error",
+    "desc" => "Fields required!"
+  ));
+}
