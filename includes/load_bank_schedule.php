d1104b8aad54ca394933363f7cead7763f0b96c6
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-22 00:59:25 +0300

    Create load_bank_schedule.php

---
+++ includes/load_bank_schedule.php
@@ -0,0 +1,26 @@
+<?php
+
+header("Content-type:application/json");
+
+include_once 'dbconnect.php';
+session_start();
+
+
+$query = "SELECT * FROM tbl_loans WHERE status='pending'";
+
+$result = mysqli_query($conn, $query);
+$response = array();
+
+while ($row = mysqli_fetch_assoc($result)) {
+
+    array_push(
+        $response,
+        array(
+            'name' => $row['bank_name']
+        )
+    );
+}
+
+echo json_encode($response);
+
+mysqli_close($conn);
