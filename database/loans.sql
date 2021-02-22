35b3629782d29fdf0eb42fe4b9d454b8c9fe4c66
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-22 09:10:55 +0300

    Update loans.sql

--- database/loans.sql
+++ database/loans.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2021 at 11:16 AM
+-- Generation Time: Feb 22, 2021 at 07:08 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.14
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tbl_bank` (
   `bank_name` varchar(50) NOT NULL,
+  `branch_name` varchar(15) NOT NULL,
   `acc_no` varchar(254) NOT NULL,
   `acc_name` varchar(50) NOT NULL,
   `currency` varchar(100) NOT NULL,
@@ -40,6 +41,14 @@ CREATE TABLE `tbl_bank` (
   `late_charges` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tbl_bank`
+--
+
+INSERT INTO `tbl_bank` (`bank_name`, `branch_name`, `acc_no`, `acc_name`, `currency`, `opening_bal`, `clear_days`, `od_limit`, `id_interest`, `over_limit`, `late_charges`) VALUES
+('EQUITY BANK', 'KARATINA', '255445666', 'JUMANJI2', 'KSHS', '200', '3', '400', '12', '3', '3'),
+('EQUITY BANK', 'KARATINA', '52656663456', 'JUMANJI', 'KSHS', '150000', '7', '1000000', '4', '1000000', '3');
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +58,14 @@ CREATE TABLE `tbl_bank` (
 CREATE TABLE `tbl_currency` (
   `currency_unit` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_currency`
+--
+
+INSERT INTO `tbl_currency` (`currency_unit`) VALUES
+('$'),
+('KSHS');
 
 -- --------------------------------------------------------
 
@@ -64,6 +81,40 @@ CREATE TABLE `tbl_customer` (
   `tel_no` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tbl_customer`
+--
+
+INSERT INTO `tbl_customer` (`name`, `email`, `physical_address`, `postal_address`, `tel_no`) VALUES
+('Byron Mcmillan', 'pahu@mailinator.com', 'Quae non cillum a et', 'Cum dolorem facilis', '+1 (436) 351-2876');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_loans`
+--
+
+CREATE TABLE `tbl_loans` (
+  `bank_name` varchar(50) NOT NULL,
+  `dis_date` date NOT NULL,
+  `first_date` date NOT NULL,
+  `amount` varchar(100) NOT NULL,
+  `period` varchar(100) NOT NULL,
+  `installment` varchar(100) NOT NULL,
+  `next_installment` varchar(100) NOT NULL,
+  `interest` varchar(100) NOT NULL,
+  `loan_category` varchar(100) NOT NULL,
+  `status` varchar(15) NOT NULL DEFAULT 'pending',
+  `late_repayment` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_loans`
+--
+
+INSERT INTO `tbl_loans` (`bank_name`, `dis_date`, `first_date`, `amount`, `period`, `installment`, `next_installment`, `interest`, `loan_category`, `status`, `late_repayment`) VALUES
+('EQUITY BANK', '2021-02-01', '2021-03-01', '100000', '10', '11200', '1', '12', 'Reducing Balance', 'pending', '5');
+
 -- --------------------------------------------------------
 
 --
@@ -76,23 +127,19 @@ CREATE TABLE `tbl_payments` (
   `cheque_no` varchar(100) NOT NULL,
   `amount` varchar(100) NOT NULL,
   `date` date NOT NULL,
-  `cheque_type` varchar(50) NOT NULL
+  `cheque_type` varchar(50) NOT NULL,
+  `pay_type` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `tbl_receipts`
+-- Dumping data for table `tbl_payments`
 --
 
-CREATE TABLE `tbl_receipts` (
-  `customer_name` varchar(50) NOT NULL,
-  `bank_name` varchar(50) NOT NULL,
-  `cheque_no` varchar(100) NOT NULL,
-  `amount` varchar(100) NOT NULL,
-  `date` date NOT NULL,
-  `cheque_type` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `tbl_payments` (`supplier_name`, `bank_name`, `cheque_no`, `amount`, `date`, `cheque_type`, `pay_type`) VALUES
+('Byron Mcmillan', 'EQUITY BANK', '833', '67', '2016-03-22', 'inhouse', 'receipt'),
+('Rajah Velasquez', 'EQUITY BANK', '342', '82', '2016-03-21', 'interbank', 'pay'),
+('Rajah Velasquez', 'EQUITY BANK', '478', '12', '2016-03-22', 'interbank', 'pay'),
+('Rajah Velasquez', 'EQUITY BANK', '965', '4', '2016-03-22', 'interbank', 'pay');
 
 -- --------------------------------------------------------
 
@@ -107,6 +154,13 @@ CREATE TABLE `tbl_supplier` (
   `postal_address` varchar(50) NOT NULL,
   `physical_address` varchar(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_supplier`
+--
+
+INSERT INTO `tbl_supplier` (`name`, `email`, `tel_no`, `postal_address`, `physical_address`) VALUES
+('Rajah Velasquez', 'zeryg@mailinator.com', '+1 (117) 557-9511', 'Non rerum officia is', 'Cumque amet possimu');
 
 --
 -- Indexes for dumped tables
@@ -131,16 +185,16 @@ ALTER TABLE `tbl_customer`
   ADD PRIMARY KEY (`name`,`email`);
 
 --
+-- Indexes for table `tbl_loans`
+--
+ALTER TABLE `tbl_loans`
+  ADD PRIMARY KEY (`bank_name`,`dis_date`);
+
+--
 -- Indexes for table `tbl_payments`
 --
 ALTER TABLE `tbl_payments`
   ADD PRIMARY KEY (`supplier_name`,`bank_name`,`cheque_no`);
-
---
--- Indexes for table `tbl_receipts`
---
-ALTER TABLE `tbl_receipts`
-  ADD PRIMARY KEY (`customer_name`,`bank_name`,`cheque_no`);
 
 --
 -- Indexes for table `tbl_supplier`
