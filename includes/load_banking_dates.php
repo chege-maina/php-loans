285b2979d8517f757bf79081b1d191253353fa72
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-25 10:52:52 +0300

    Update load_banking_dates.php

--- includes/load_banking_dates.php
+++ includes/load_banking_dates.php
@@ -20,32 +20,76 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
       $open_bal = $row3['opening_bal'];
     }
+    $valid_date = $date;
+    $money_out = 0;
+    $money_in = 0;
+    $amount = 0;
+    $detail = 'NONE';
+    $cheque_no = 0;
+    $balance = $open_bal + $money_in - $money_out;
+    $checks = 1;
     $query2 = "SELECT * FROM tbl_payments WHERE bank_name='$bank' and date='$date' and status='pending'";
     $result2 = mysqli_query($conn, $query2);
     $response2 = array();
     while ($row2 = mysqli_fetch_assoc($result2)) {
       $cheque_type = $row2['cheque_type'];
+      $checks = 0;
+
       if (strcmp($cheque_type, 'interbank') == 0) {
         $valid_date = date('Y-m-d', strtotime($date . ' + ' . $clear_days . ' days'));
+      } else if (is_null($cheque_type)) {
+        $valid_date = $date;
       } else {
         $valid_date = $date;
       }
+
       $pay_type = $row2['pay_type'];
       if (strcmp($pay_type, 'pay') == 0) {
         $money_in = 0;
         $money_out = $row2['amount'];
+      } else if (is_null($pay_type)) {
+        $money_out = 0;
+        $money_in = 0;
       } else {
         $money_out = 0;
         $money_in = $row2['amount'];
+      }
+      $amount = $row2['amount'];
+      if (is_null($amount)) {
+        $amount = 0;
+      }
+      $detail = $row2['supplier_name'];
+      if (is_null($detail)) {
+        $detail = 'NONE';
+      }
+      $cheque_no = $row2['cheque_no'];
+      if (is_null($cheque_no)) {
+        $cheque_no = 0;
       }
       $balance = $open_bal + $money_in - $money_out;
       array_push(
         $response2,
         array(
           'banking_date' => $date,
-          'cheque_no' => $row2['cheque_no'],
-          'details' => $row2['supplier_name'],
-          'amount' => $row2['amount'],
+          'cheque_no' => $cheque_no,
+          'details' => $detail,
+          'amount' => $amount,
+          'value_date' => $valid_date,
+          'money_in' => $money_in,
+          'money_out' => $money_out,
+          'balance' => $balance
+
+        )
+      );
+    }
+    if ($checks == 1) {
+      array_push(
+        $response2,
+        array(
+          'banking_date' => $date,
+          'cheque_no' => $cheque_no,
+          'details' => $detail,
+          'amount' => $amount,
           'value_date' => $valid_date,
           'money_in' => $money_in,
           'money_out' => $money_out,
