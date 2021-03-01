e97ae2e9a797f2c0af03f9fb416a80086068e1e4
Timothy Karani <kartimothy@gmail.com>
2021-03-01 13:28:59 +0300

    Adding user to DB

--- auth/add_user.php
+++ auth/add_user.php
@@ -43,7 +43,7 @@ include "../includes/base_page/shared_top_tags.php"
         <div class="field">
           <label class="label">Password</label>
           <div class="control">
-            <input class="input" type="text" id="password" required placeholder="Password">
+            <input class="input" type="password" id="password" required placeholder="Password">
           </div>
         </div>
       </div>
@@ -52,7 +52,7 @@ include "../includes/base_page/shared_top_tags.php"
         <div class="field">
           <label class="label">Repeat Password</label>
           <div class="control">
-            <input class="input" type="text" id="repeat_password" required placeholder="Repeat Password">
+            <input class="input" type="password" id="repeat_password" required placeholder="Repeat Password">
           </div>
         </div>
       </div>
@@ -113,6 +113,34 @@ include "../includes/base_page/shared_top_tags.php"
       repeat_password.focus();
       return false;
     }
+
+    const formData = new FormData();
+    formData.append("first_name", first_name.value);
+    formData.append("last_name", last_name.value);
+    formData.append("email", email.value);
+    formData.append("password", password.value);
+    formData.append("designation", designation.value);
+    formData.append("branch", branch.value);
+
+    fetch('../includes/add_user.php', {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => response.json())
+      .then(result => {
+        if (result["message"] === "success") {
+          showSuccessAlert("User added successfuly");
+          window.setTimeout(() => {
+            location.reload();
+          }, 2500);
+        } else {
+          showDangerAlert("User not added");
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+
     return false;
   }
 
