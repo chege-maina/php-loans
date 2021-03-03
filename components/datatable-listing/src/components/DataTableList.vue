15877bae8f3190d6f0ab92c34547089f31b6da4e
Timothy Karani <kartimothy@gmail.com>
2021-03-03 08:18:57 +0300

    Datatable component working

--- components/datatable-listing/src/components/DataTableList.vue
+++ components/datatable-listing/src/components/DataTableList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="table-responsive">
+  <div class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
     <table class="table table-sm table-striped table-hover">
       <thead>
         <tr>
@@ -25,9 +25,9 @@
 
           <template v-for="(value, key) in item">
             <td v-bind:key="key" v-if="key !== 'key'" class="align-middle py-1">
-              <span v-if="header_object[key].editable">
+              <span v-if="header_object[key].editable" class="control">
                 <input
-                  class="form-control form-control-sm"
+                  class="input is-small"
                   type="number"
                   v-model="table_data[item.key][key]"
                 />
@@ -40,24 +40,7 @@
           </template>
 
           <td class="align-middle py-1">
-            <button
-              class="btn btn-falcon-default btn-sm rounded-pill px-1 py-0"
-              type="button"
-              v-on:click="removeRow(item.key)"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-x"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
-                />
-              </svg>
-            </button>
+            <button class="delete" v-on:click="removeRow(item.key)"></button>
           </td>
         </tr>
       </tbody>
@@ -198,6 +181,5 @@ export default {
 };
 </script>
 <style>
-@import url("http://localhost:5000/assets/css/theme-rtl.min.css");
-@import url("http://localhost:5000/assets/css/theme.min.css");
+@import url("http://localhost:5000/external/bulma/bulma.min.css");
 </style>
--- customer/list_customer.php
+++ customer/list_customer.php
@@ -55,6 +55,206 @@ include "../includes/base_page/shared_top_tags.php"
   </div>
 </div>
 
+<div class="card mt-1">
+  <script src="https://unpkg.com/vue"></script>
+  <script src="../components/datatable-listing/dist/datatable-list.min.js"></script>
+  <div id="datatable">
+  </div>
+  <script>
+    const headers = [{
+        name: "key",
+        editable: false,
+        key: "key",
+        computed: false
+      },
+      {
+        name: "Name",
+        editable: false,
+        key: "name",
+        computed: false
+      },
+      {
+        name: "Code",
+        editable: false,
+        key: "code",
+        computed: false
+      },
+      {
+        name: "Stock",
+        editable: false,
+        key: "stock",
+        computed: false
+      },
+      {
+        name: "Quantity",
+        editable: true,
+        key: "quantity",
+        computed: false
+      },
+      {
+        name: "Price",
+        editable: false,
+        key: "price",
+        computed: false
+      },
+      {
+        name: "Discount",
+        editable: true,
+        key: "discount",
+        computed: false
+      },
+      {
+        name: "Tax %",
+        editable: false,
+        key: "tax_pc",
+        computed: false
+      },
+      {
+        name: "Tax",
+        editable: false,
+        key: "tax",
+        computed: true,
+        operation: "tax_pc / 100 * quantity * price",
+      },
+      {
+        name: "Subtotal",
+        editable: false,
+        key: "subtotal",
+        computed: true,
+        operation: "tax_pc / 100 + 1 * quantity * price",
+      },
+    ];
+    const items = [{
+        key: 1,
+        name: "Wiberg Super Cure",
+        code: "LT38 2725 3405 4331 5052",
+        stock: 59,
+        quantity: 85,
+        price: 13,
+        discount: 40,
+        tax_pc: 17,
+        tax: -1,
+        subtotal: 60054,
+      },
+      {
+        key: 2,
+        name: "Bacardi Breezer - Strawberry",
+        code: "PS97 QVMV EN6O KQNI FXSD SPHX ICTY H",
+        stock: 23,
+        quantity: 59,
+        price: 68,
+        discount: 84,
+        tax_pc: 3,
+        tax: -1,
+        subtotal: 784,
+      },
+      {
+        key: 3,
+        name: "Avocado",
+        code: "LT21 2680 9565 6475 1512",
+        stock: 26,
+        quantity: 90,
+        price: 84,
+        discount: 2,
+        tax_pc: 3,
+        tax: -1,
+        subtotal: 57305,
+      },
+      {
+        key: 4,
+        name: "Soup - Knorr, Country Bean",
+        code: "MR92 5543 6765 4828 5308 1513 081",
+        stock: 72,
+        quantity: 73,
+        price: 13,
+        discount: 61,
+        tax_pc: 15,
+        tax: -1,
+        subtotal: 15940,
+      },
+      {
+        key: 5,
+        name: "Kiwi",
+        code: "CY32 4469 2631 OPJT FDCH I1HF 9WEQ",
+        stock: 33,
+        quantity: 73,
+        price: 69,
+        discount: 31,
+        tax_pc: 15,
+        tax: -1,
+        subtotal: 15544,
+      },
+      {
+        key: 6,
+        name: "Banana - Green",
+        code: "PT08 0202 1850 4474 9222 7254 5",
+        stock: 99,
+        quantity: 87,
+        price: 54,
+        discount: 100,
+        tax_pc: 7,
+        tax: -1,
+        subtotal: 77459,
+      },
+      {
+        key: 7,
+        name: "Bread - Burger",
+        code: "CR19 0596 3701 4238 7857 0",
+        stock: 85,
+        quantity: 69,
+        price: 74,
+        discount: 79,
+        tax_pc: 11,
+        tax: -1,
+        subtotal: 91874,
+      },
+      {
+        key: 8,
+        name: "Bouq All Italian - Primerba",
+        code: "FI31 5087 0293 5633 84",
+        stock: 53,
+        quantity: 85,
+        price: 58,
+        discount: 31,
+        tax_pc: 1,
+        tax: -1,
+        subtotal: 78031,
+      },
+      {
+        key: 9,
+        name: "Beef - Tender Tips",
+        code: "GT18 SXNS QA5F D7D9 B8P7 TYNO 83BV",
+        stock: 72,
+        quantity: 100,
+        price: 20,
+        discount: 88,
+        tax_pc: 1,
+        tax: -1,
+        subtotal: 14014,
+      },
+      {
+        key: 10,
+        name: "Sachet",
+        code: "MD71 SAAH ZICA HQPH 1JQP 3DEB",
+        stock: 76,
+        quantity: 53,
+        price: 55,
+        discount: 54,
+        tax_pc: 10,
+        tax: -1,
+        subtotal: 10191,
+      },
+    ];
+    window.addEventListener('DOMContentLoaded', (event) => {
+      const datatable = document.querySelector("#datatable");
+      const elem = document.createElement("datatable-list");
+      elem.setAttribute("json_header", JSON.stringify(headers));
+      elem.setAttribute("json_items", JSON.stringify(items));
+      datatable.appendChild(elem);
+    });
+  </script>
+</div>
+
 <script>
   const customer_name = document.querySelector('#customer_name');
   const table_body = document.querySelector('#table_body');
