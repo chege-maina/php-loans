da0a50ba1404c5bf5f54c4797d63ac7a9132c6fa
Timothy Karani <kartimothy@gmail.com>
2021-03-03 08:54:11 +0300

    Cleaned up ui

--- customer/list_customer.php
+++ customer/list_customer.php
@@ -31,29 +31,6 @@ include "../includes/base_page/shared_top_tags.php"
     </div>
   </div>
 </div>
-<div class="card mt-1">
-  <div class="card-content">
-    <hr>
-    <div class="table-container">
-      <table class="table is-hoverable is-fullwidth is-striped">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Physical Address</th>
-            <th>Postal Address</th>
-            <th>Telephone Number</th>
-          </tr>
-        </thead>
-        <tbody id="table_body">
-        </tbody>
-        <tfoot id="table_foot">
-        </tfoot>
-      </table>
-      <!-- Content ends here -->
-    </div>
-  </div>
-</div>
 
 <div class="card mt-1">
   <script src="https://unpkg.com/vue"></script>
@@ -70,47 +47,7 @@ include "../includes/base_page/shared_top_tags.php"
 
 
   let updateTable = (data) => {
-    table_body.innerHTML = "";
-    let i = 0;
-    data.forEach(value => {
-      const this_row = document.createElement("tr");
-
-      const name = document.createElement("td");
-      name.appendChild(document.createTextNode(value["name"]));
-      name.classList.add("align-middle");
-
-      const email = document.createElement("td");
-      email.appendChild(document.createTextNode(value["email"]));
-      email.classList.add("align-middle");
-
-      const physical_address = document.createElement("td");
-      physical_address.appendChild(document.createTextNode(value["physical_address"]));
-      physical_address.classList.add("align-middle");
-
-      const postal_address = document.createElement("td");
-      postal_address.appendChild(document.createTextNode(value["postal_address"]));
-      postal_address.classList.add("align-middle");
-
-      const tel_no = document.createElement("td");
-      tel_no.appendChild(document.createTextNode(value["tel_no"]));
-      tel_no.classList.add("align-middle");
-
-      //  const req_actions = document.createElement("td");
-      //const btn = document.createElement("button");
-
-      // btn.setAttribute("onclick", "detailedView(" + i + ")");
-      // if (i > 0) {
-      // btn.setAttribute("disabled", "");
-      //  }
-      //  i++;
-      //  btn.appendChild(document.createTextNode("Manage"));
-      //   btn.classList.add("button", "is-small", "is-info");
-      //    req_actions.appendChild(btn);
-
-      this_row.append(name, email, physical_address, postal_address, tel_no);
-      table_body.appendChild(this_row);
-    });
-
+    console.log(data);
   }
 
   // function detailedView(i) {
@@ -195,6 +132,17 @@ include "../includes/base_page/shared_top_tags.php"
       .then(result => {
         table_items = result;
         updateTable(result);
+        createHeaders(result);
+        createItems(result);
+        updateTable(result);
+        const datatable = document.querySelector("#datatable");
+        const elem = document.createElement("datatable-list");
+        elem.setAttribute("json_header", JSON.stringify(table_headers));
+        elem.setAttribute("json_items", JSON.stringify(table_items_c));
+        elem.classList.add("is-fullwidth");
+        datatable.appendChild(elem);
+
+
       })
       .catch(error => {
         console.error('Error:', error);
