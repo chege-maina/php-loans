a30ac88d88f4af01198365ec5a23143042159fa2
Timothy Karani <kartimothy@gmail.com>
2021-03-03 16:19:20 +0300

    Base UIs added

---
+++ banks/edit_bank.php
@@ -0,0 +1,220 @@
+<?php
+include "../includes/base_page/shared_top_tags.php"
+?>
+
+<div class="block title">
+  Add New Bank
+</div>
+
+<div class="box">
+
+  <form id="form_tag" onsubmit="return submitForm();">
+    <div class="columns">
+
+      <div class="column">
+        <label for="bank_name" class="label">Name</label>
+        <div class="control">
+          <input type="text" name="bank_name" id="bank_name" class="input" required placeholder="Bank name">
+        </div>
+      </div>
+
+      <div class="column">
+        <label for="branch_name" class="label">Branch</label>
+        <div class="control">
+          <input type="text" name="branch_name" id="branch_name" class="input" required placeholder="Branch name">
+        </div>
+      </div>
+
+      <div class="column">
+        <label for="account_number" class="label">Account Number</label>
+        <div class="control">
+          <input type="number" class="input" id="account_number" required placeholder="Account number">
+        </div>
+      </div>
+
+      <div class="column">
+        <label for="account_number" class="label">Account Name</label>
+        <div class="control">
+          <input type="text" name="account_name" id="account_name" class="input" required placeholder="Account name">
+        </div>
+      </div>
+
+    </div>
+    <hr>
+    <div class="columns">
+
+      <div class="column">
+        <label for="opening_balance" class="label">Opening Balance</label>
+        <div class="control commify">
+          <input type="text" class="input" required placeholder="Opening balance" data-commify="opening_balance">
+        </div>
+      </div>
+      <div class="column">
+        <label for="date" class="label"> As of Date</label>
+        <!-- autofill current date  -->
+        <div class="control">
+          <input type="date" value="<?php echo date("Y-m-d"); ?>" id="p_date" class="input is-link" required>
+        </div>
+      </div>
+      <div class="column">
+        <label for="currency" class="label">Currency</label>
+        <div class="control">
+          <div class="select is-fullwidth">
+            <select id="currency" name="currency">
+            </select>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="column">
+        <label for="cheque_clear_days" class="label">Cheque Clear Days</label>
+        <div class="control">
+          <input type="number" name="cheque_clear_days" id="cheque_clear_days" class="input" required placeholder="Cheque clear days" max="15">
+        </div>
+      </div>
+
+    </div>
+    <hr>
+    <div class="columns">
+
+      <div class="column is-fullwidth">
+        <label for="overdraft_interest" class="label">Overdraft Interest</label>
+        <div class="field has-addons">
+          <p class="control">
+            <input type="number" name="overdraft_interest" id="overdraft_interest" class="input" required placeholder="Overdraft interest" max="100">
+          </p>
+          <p>
+            <a class="button is-static">%</a>
+          </p>
+        </div>
+      </div>
+
+
+      <div class="column is-fullwidth">
+        <label for="overlimit_interest" class="label">Overdraft Limit Interest</label>
+        <div class="field has-addons">
+          <p class="control">
+            <input type="number" name="overlimit_interest" id="overlimit_interest" class="input" required placeholder="Overdraft Limit interest" max="100">
+          </p>
+          <p class="control">
+            <a>
+              <a class="button is-static">%</a>
+            </a>
+          </p>
+        </div>
+      </div>
+
+      <div class="column is-fullwidth">
+        <label for="late_payment_charges" class="label">Late Payment Charges</label>
+        <div class="control">
+          <div class="field has-addons">
+            <p class="control">
+              <input type="number" name="late_payment_charges" id="late_payment_charges" class="input" required placeholder="Late payment charges" max="100">
+            </p>
+            <p class="control">
+              <a>
+                <a class="button is-static">%</a>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="column">
+        <label for="overdraft_limit" class="label">Overdraft Limit</label>
+        <div class="control">
+          <input type="number" name="overdraft_limit" id="overdraft_limit" class="input" required placeholder="Overdraft charges">
+        </div>
+      </div>
+
+    </div>
+
+    <div class="field is-grouped">
+      <div class="control">
+        <button class="button is-link">Submit</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+<script>
+  const form_tag = document.querySelector("#form_tag");
+
+  const bank_name = document.querySelector("#bank_name");
+  const branch_name = document.querySelector("#branch_name");
+  const account_name = document.querySelector("#account_name");
+  const account_number = document.querySelector("#account_number");
+
+  const cheque_clear_days = document.querySelector("#cheque_clear_days");
+  const currency = document.querySelector("#currency");
+
+  const overdraft_interest = document.querySelector("#overdraft_interest");
+  const overlimit_interest = document.querySelector("#overlimit_interest");
+  const overdraft_limit = document.querySelector("#overdraft_limit");
+  const late_payment_charges = document.querySelector("#late_payment_charges");
+  const p_date = document.querySelector("#p_date");
+
+
+  let opening_balance;
+
+  function submitForm() {
+
+    console.log("Submitting");
+    const formData = new FormData();
+    formData.append("bank_name", bank_name.value);
+    formData.append("acc_no", account_number.value);
+    formData.append("acc_name", account_name.value);
+    formData.append("currency", currency.value);
+    formData.append("opening_bal", opening_balance.value);
+    formData.append("clear_days", cheque_clear_days.value);
+    formData.append("od_limit", overdraft_limit.value);
+    formData.append("id_interest", overdraft_interest.value);
+    formData.append("over_limit", overlimit_interest.value);
+    formData.append("branch", branch_name.value);
+    formData.append("date", p_date.value);
+    formData.append("late_charges", late_payment_charges.value);
+
+    fetch('../includes/add_bank.php', {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => response.json())
+      .then(result => {
+        console.log('Success:', result);
+        if (result["message"] === "success") {
+          showSuccessAlert("Record stored successfuly");
+          reloadPage();
+        } else {
+          let msg = !("desc" in result) || result["desc"].trim() === "" ?
+            "Record not stored" : result["desc"];
+          showDangerAlert(msg);
+          removeAlert();
+        }
+      })
+      .catch(error => {
+        console.log(error)
+        showDangerAlert("Could not send data to server");
+        removeAlert();
+        // reloadPage(5000);
+      });
+
+    return false;
+  }
+
+
+  window.addEventListener('DOMContentLoaded', (event) => {
+
+    initSelectElement("#currency", "-- Select Currency --");
+    populateSelectElement("#currency", '../includes/load_currency.php', "name");
+
+    commifyAll();
+    opening_balance = document.querySelector("#opening_balance");
+
+
+  });
+</script>
+
+<?php
+include "../includes/base_page/shared_bottom_tags.php"
+?>
---
+++ receipts/edit_receipts.php
@@ -0,0 +1,154 @@
+<?php
+include "../includes/base_page/shared_top_tags.php"
+?>
+
+<div class="block title">
+  Receive Payment
+</div>
+<form onsubmit="return submitForm();">
+  <div class="card">
+
+    <div class="card-content">
+      <!-- Content is to start here -->
+      <div class="columns">
+
+        <div class="column">
+          <label for="customer" class="label">Select Customer</label>
+          <div class="select is-fullwidth  ">
+            <div class="control">
+              <select id="customer" required>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <label for="bank" class="label">Select Bank</label>
+          <div class="select is-fullwidth required">
+            <div class="control">
+              <select id="bank" required>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <label for="date" class="label">Select Payment Date</label>
+          <!-- autofill current date  -->
+          <div class="control">
+            <input type="date" value="<?php echo date("Y-m-d"); ?>" id="p_date" class="input is-link" required>
+          </div>
+        </div>
+
+      </div>
+
+      <hr>
+
+      <div class="columns">
+        <div class="column">
+          <label for="cheque_number" class="label">Enter Cheque Number</label>
+          <div class="control">
+            <input type="number" name="cheque_number" id="cheque_number" class="input" required placeholder="Cheque Number" required>
+          </div>
+        </div>
+
+        <div class="column">
+          <label for="amount_banked" class="label">Amount Banked</label>
+          <div class="control">
+            <input type="number" name="amount_paid" id="amount_banked" class="input" required placeholder="Amount Paid" required>
+          </div>
+        </div>
+
+        <div class="column">
+          <label for="payment_type" class="label">Select Cheque Type</label>
+          <div class="select is-fullwidth" required>
+            <select id="cheque_type" required>
+              <option value disabled selected>-- Select Cheque Type --</option>
+              <option value="inhouse">Inhouse</option>
+              <option value="interbank">Interbank</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div class="columns">
+        <div class="column">
+          <button class="button is-link">Submit</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</form>
+
+<script>
+  window.addEventListener('DOMContentLoaded', (event) => {
+    initSelectElement("#customer", "-- Select Customer --");
+    populateSelectElement("#customer", "../includes/load_customer.php", "name");
+
+
+    initSelectElement("#bank", "-- Select Bank --");
+    populateSelectElement("#bank", "../includes/load_bank.php", "name");
+  });
+
+
+  const customer = document.querySelector("#customer");
+  const bank = document.querySelector("#bank");
+  const p_date = document.querySelector("#p_date");
+  const cheque_number = document.querySelector("#cheque_number");
+  const amount_banked = document.querySelector("#amount_banked");
+  const cheque_type = document.querySelector("#cheque_type");
+
+  function submitForm() {
+
+    console.log("submitting");
+
+
+    const formData = new FormData();
+
+    console.log("====================================");
+    console.log("customer_name", customer.value);
+    console.log("bank_name", bank.value);
+    console.log("date", p_date.value);
+    console.log("cheque_type", cheque_type.value);
+    console.log("cheque_no", cheque_number.value);
+    console.log("amount", amount_banked.value);
+    console.log("====================================");
+
+
+    formData.append("customer_name", customer.value);
+    formData.append("bank_name", bank.value);
+    formData.append("date", p_date.value);
+    formData.append("cheque_type", cheque_type.value);
+    formData.append("cheque_no", cheque_number.value);
+    formData.append("amount", amount_banked.value);
+
+    fetch('../includes/add_receipt.php', {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => response.json())
+      .then(result => {
+        console.log('Success:', result);
+        if (result["message"] === "success") {
+          showSuccessAlert("Record stored successfuly");
+          reloadPage();
+        } else {
+          let msg = !("desc" in result) || result["desc"].trim() === "" ?
+            "Record not stored" : result["desc"];
+          showDangerAlert(msg);
+          removeAlert();
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        showDangerAlert("Could not send data to server");
+        removeAlert();
+      });
+
+    return false;
+  }
+</script>
+
+<?php
+include "../includes/base_page/shared_bottom_tags.php"
+?>
