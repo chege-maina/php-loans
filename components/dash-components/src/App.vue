1b13a8bd7a8f213ad06f7d067e7f7b5fc57576f6
Timothy Karani <kartimothy@gmail.com>
2021-03-03 23:44:12 +0300

    Added base card code

--- components/dash-components/src/App.vue
+++ components/dash-components/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <PercentageLarge />
+    <CreditCards />
   </div>
 </template>
 
 <script>
 import PercentageLarge from "./components/PercentageLarge.vue";
+import CreditCards from "./components/CreditCards.vue";
 
 export default {
   name: "App",
   components: {
     PercentageLarge,
+    CreditCards,
   },
 };
 </script>
