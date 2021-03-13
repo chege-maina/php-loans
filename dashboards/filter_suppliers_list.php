1e4cdd50a017c22dadc417d84461f17b2d4a217a
Chege Maina <mr.shei8@gmail.com>
2021-03-13 17:57:04 +0300

    Create filter_suppliers_list.php

---
+++ dashboards/filter_suppliers_list.php
@@ -0,0 +1,37 @@
+<?php
+
+header("Content-type:application/json");
+
+include_once 'dbconnect.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $supplier = $_POST["supplier"];
+
+  $query = "SELECT * FROM tbl_supplier WHERE name='$supplier'";
+
+  $result = mysqli_query($conn, $query);
+  $response = array();
+
+  while ($row = mysqli_fetch_assoc($result)) {
+    $name = $row['name'];
+    $email = $row['email'];
+    $physical_address = $row['physical_address'];
+    $postal_address = $row['postal_address'];
+    $tel_no = $row['tel_no'];
+
+    array_push(
+      $response,
+      array(
+        'name' => $name,
+        'email' => $email,
+        'physical_address' => $physical_address,
+        'postal_address' => $postal_address,
+        'tel_no' => $tel_no
+
+      )
+    );
+  }
+
+  echo json_encode($response);
+}
+
+mysqli_close($conn);
