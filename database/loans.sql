196a9a861e0ea9da8f9547168611b9944eb5e1d6
Chege Maina <mr.shei8@gmail.com>
2021-03-13 22:42:56 +0300

    Update loans.sql

--- database/loans.sql
+++ database/loans.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2021 at 08:43 AM
+-- Generation Time: Mar 13, 2021 at 08:20 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.14
 
