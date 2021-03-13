32215b32651d1e06712b441cd65d57508014b861
Chege Maina <mr.shei8@gmail.com>
2021-03-13 18:28:01 +0300

    value date

--- includes/load_payments_list.php
+++ includes/load_payments_list.php
@@ -18,6 +18,16 @@ while ($row = mysqli_fetch_assoc($result)) {
   $cheque_no = $row['cheque_no'];
   $amount = $row['amount'];
   $cheque_type = $row['cheque_type'];
+  if (strcmp($cheque_type, 'interbank') == 0) {
+    $query2 = "SELECT * FROM tbl_bank WHERE bank_name='$bank'";
+    $result2 = mysqli_query($conn, $query2);
+    if ($row2 = mysqli_fetch_assoc($result2)) {
+      $clear_days = $row2['clear_days'];
+      $valid_date = date('Y-m-d', strtotime($date . ' + ' . $clear_days . ' days'));
+    }
+  } else {
+    $valid_date = $date;
+  }
 
   array_push(
     $response,
@@ -26,6 +36,7 @@ while ($row = mysqli_fetch_assoc($result)) {
       'supplier' => $supplier,
       'cheque_no' => $cheque_no,
       'amount' => $amount,
+      'Value_Date' => $valid_date,
       'cheque_type' => $cheque_type
 
     )
