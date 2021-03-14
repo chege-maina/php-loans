c895a9b75f200cb22c68abf7453fddb64ec66839
Timothy Karani <kartimothy@gmail.com>
2021-03-14 22:57:03 +0300

    Countup components ready for use

--- components/fdash-components/src/components/CountUpOrangey.vue
+++ components/fdash-components/src/components/CountUpOrangey.vue
@@ -10,19 +10,18 @@
 
     <div class="card-body position-relative">
       <h6>
-        Customers<span class="badge badge-soft-warning rounded-pill ml-2"
-          >-0.23%</span
-        >
+        {{ title }}
+        <span class="badge badge-soft-warning rounded-pill ml-2">
+          {{ title_chip }}
+        </span>
       </h6>
       <div
         class="display-4 fs-4 mb-2 fw-normal font-sans-serif text-warning"
-        data-countup='{"endValue":58.386,"decimalPlaces":2,"suffix":"k"}'
+        v-bind:data-countup="countup_json"
       >
-        58.39k
+        {{ prefix }} {{ end_value }}
       </div>
-      <a class="fw-semi-bold fs--1 text-nowrap" href="e-commerce/customers.html"
-        >See all &raquo;</a
-      >
+      <a class="fw-semi-bold fs--1 text-nowrap">{{ footer }} &raquo; </a>
     </div>
   </div>
 </template>
@@ -30,80 +29,97 @@
 <script>
 export default {
   props: {
-    name: { type: String, default: "Jean" },
-    mounted() {
-      const falcon_js = document.createElement("script");
-      falcon_js.setAttribute(
-        "src",
-        "https://qonsolidated-solutions.github.io/falcon-assets/assets/js/theme.min.js"
-      );
-      const anchor_js = document.createElement("script");
-      anchor_js.setAttribute(
-        "src",
-        "https://qonsolidated-solutions.github.io/falcon-assets/vendors/anchorjs/anchor.min.js"
-      );
-      const popper = document.createElement("script");
-      popper.setAttribute(
-        "src",
-        "https://qonsolidated-solutions.github.io/falcon-assets/vendors/popper/popper.min.js"
-      );
-      const bootstrap = document.createElement("script");
-      bootstrap.setAttribute(
-        "src",
-        "https://qonsolidated-solutions.github.io/falcon-assets/vendors/bootstrap/bootstrap.min.js"
-      );
-      const is_js = document.createElement("script");
-      is_js.setAttribute("src", "https://qonsolidated-solutions.github.io/falcon-assets/vendors/is/is.min.js");
-      const prism = document.createElement("script");
-      prism.setAttribute("src", "https://qonsolidated-solutions.github.io/falcon-assets/vendors/prism/prism.js");
-      const fontawesome = document.createElement("script");
-      fontawesome.setAttribute(
-        "src",
-        "https://qonsolidated-solutions.github.io/falcon-assets/vendors/fontawesome/all.min.js"
-      );
-      const lodash = document.createElement("script");
-      lodash.setAttribute(
-        "src",
-        "https://qonsolidated-solutions.github.io/falcon-assets/vendors/lodash/lodash.min.js"
-      );
-      const polyfill = document.createElement("script");
-      polyfill.setAttribute(
-        "src",
-        "https://polyfill.io/v3/polyfill.min.js?features,window.scroll"
-      );
-      const list_js = document.createElement("script");
-      list_js.setAttribute(
-        "src",
-        "https://qonsolidated-solutions.github.io/falcon-assets/vendors/list.js/list.min.js"
-      );
-      const config_js = document.createElement("script");
-      config_js.setAttribute(
-        "src",
-        "https://qonsolidated-solutions.github.io/falcon-assets/assets/js/config.js"
-      );
-
-      this.$el.prepend(config_js);
-      this.$el.append(
-        anchor_js,
-        popper,
-        bootstrap,
-        is_js,
-        prism,
-        fontawesome,
-        lodash,
-        polyfill,
-        list_js,
-        falcon_js
-      );
-
-      this.i_total = this.body.length / this.per_page;
-      this.i_total = this.i_total < 1 ? 1 : this.i_total;
-
-      this.i_total =
-        this.i_total.toString().split(".").length > 1
-          ? Number(this.i_total.toString().split(".")[0]) + 1
-          : this.i_total;
+    title: { type: String, default: "Rays" },
+    title_chip: { type: String, default: "10.1%" },
+    end_value: { type: Number, default: 53421 },
+    prefix: { type: String, default: "KSh." },
+    footer: { type: String, default: "Photons" },
+  },
+  computed: {
+    countup_json: function () {
+      let tmp = {
+        endValue: this.end_value,
+        prefix: this.prefix,
+      };
+      return JSON.stringify(tmp);
     },
+  },
+  mounted() {
+    const falcon_js = document.createElement("script");
+    falcon_js.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/assets/js/theme.min.js"
+    );
+    const anchor_js = document.createElement("script");
+    anchor_js.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/anchorjs/anchor.min.js"
+    );
+    const popper = document.createElement("script");
+    popper.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/popper/popper.min.js"
+    );
+    const bootstrap = document.createElement("script");
+    bootstrap.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/bootstrap/bootstrap.min.js"
+    );
+    const is_js = document.createElement("script");
+    is_js.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/is/is.min.js"
+    );
+    const prism = document.createElement("script");
+    prism.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/prism/prism.js"
+    );
+    const fontawesome = document.createElement("script");
+    fontawesome.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/fontawesome/all.min.js"
+    );
+    const lodash = document.createElement("script");
+    lodash.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/lodash/lodash.min.js"
+    );
+    const polyfill = document.createElement("script");
+    polyfill.setAttribute(
+      "src",
+      "https://polyfill.io/v3/polyfill.min.js?features,window.scroll"
+    );
+    const list_js = document.createElement("script");
+    list_js.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/list.js/list.min.js"
+    );
+    const config_js = document.createElement("script");
+    config_js.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/assets/js/config.js"
+    );
+    const countup = document.createElement("script");
+    countup.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/countup/countUp.umd.js"
+    );
+
+    this.$el.prepend(config_js);
+    this.$el.append(
+      countup,
+      anchor_js,
+      popper,
+      bootstrap,
+      is_js,
+      prism,
+      fontawesome,
+      lodash,
+      polyfill,
+      list_js,
+      falcon_js
+    );
   },
 };
 </script>
