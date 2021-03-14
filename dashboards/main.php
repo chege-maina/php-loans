5b5234088e5700846cdf41491b929bdcf7b6fed2
Timothy Karani <kartimothy@gmail.com>
2021-03-15 00:26:55 +0300

    Added a couple of graphs

--- dashboards/main.php
+++ dashboards/main.php
@@ -18,19 +18,37 @@ include "../includes/base_page/shared_top_tags.php"
     <fdash-count-up-orangey></fdash-count-up-orangey>
   </div>
 </div>
+
 <div class="row">
   <div class="col">
     <div class="card">
+      <h6 class="card-header bg-light">Opening Balance</h6>
+      <div class=" card-body position-relative">
+        <canvas id="opening_bal" width="200" height="50"></canvas>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row mt-2">
+  <div class="col">
+    <div class="card">
+      <h6 class="card-header bg-light">Closing Balance</h6>
       <div class=" card-body position-relative">
         <canvas id="closing_bal" width="150" height="40"></canvas>
       </div>
     </div>
   </div>
 </div>
-<div class="row">
+
+
+<div class="row mt-2">
   <div class="col">
-    <div class="card mt-2 p-2">
-      <canvas id="opening_bal" width="200" height="50"></canvas>
+    <div class="card">
+      <h6 class="card-header bg-light">Running Balance</h6>
+      <div class=" card-body position-relative">
+        <canvas id="running_bal" width="200" height="50"></canvas>
+      </div>
     </div>
   </div>
 </div>
@@ -39,10 +57,8 @@ include "../includes/base_page/shared_top_tags.php"
     fetch('./dashboard_od_closingbal.php')
       .then(response => response.json())
       .then(data => {
-        console.log(data);
         let headers = data.map(row => row.bank + "\n" + row.date);
         let values = data.map(row => row["closing balance"]);
-        console.log(headers, values);
         drawClosingBalances(headers, values);
       })
       .catch((error) => {
@@ -52,11 +68,29 @@ include "../includes/base_page/shared_top_tags.php"
     fetch('./dashboard_od_openingbal.php')
       .then(response => response.json())
       .then(data => {
-        console.log(data);
         let headers = data.map(row => row.bank + "\n" + row.date);
         let values = data.map(row => row["opening balance"]);
-        console.log(headers, values);
         drawOpeningBal(headers, values);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+
+    fetch('./dashboard_od_stuff.php')
+      .then(response => response.json())
+      .then(data => {
+        let headers = data.map(row => row.bank + "\n" + row.date);
+        let values = data.map(row => row["Running_balance"]);
+        drawRunningBal(headers, values);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+
+    fetch('./dashboard_od_totaltransactions.php')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -70,24 +104,36 @@ include "../includes/base_page/shared_top_tags.php"
       data: {
         labels: headers,
         datasets: [{
-          label: 'opening Balance',
+          label: 'opening balance',
           data: values,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
+  function drawRunningBal(headers, values) {
+    var ctx = document.getElementById('running_bal').getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: headers,
+        datasets: [{
+          label: 'opening balance',
+          data: values,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
           borderWidth: 1
         }]
       },
@@ -113,22 +159,8 @@ include "../includes/base_page/shared_top_tags.php"
         datasets: [{
           label: 'Closing Balance',
           data: values,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
           borderWidth: 1
         }]
       },
