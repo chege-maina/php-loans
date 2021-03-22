fb25bcb9eacf9e271e3e7777f8ffb12162b635d0
Timothy Karani <kartimothy@gmail.com>
2021-03-23 00:05:26 +0300

    Can show the values

---
+++ dashboards/get_single_value_dict.php
@@ -0,0 +1,43 @@
+<?php
+
+header("Content-type:application/json");
+
+include_once '../includes/dbconnect.php';
+
+$query = "SELECT COUNT(supplier_name) as count FROM tbl_payments WHERE pay_type='pay'";
+
+$query2 = "SELECT COUNT(supplier_name) as count FROM tbl_payments WHERE pay_type='receipt'";
+
+$result = mysqli_query($conn, $query);
+
+$result2 = mysqli_query($conn, $query2);
+
+$response = array();
+
+if ($row = mysqli_fetch_assoc($result)) {
+  array_push(
+    $response,
+    array(
+      'type' => 'pay',
+      'count' => $row['count'],
+    )
+  );
+}
+
+
+if ($row = mysqli_fetch_assoc($result2)) {
+  array_push(
+    $response,
+    array(
+      'type' => 'receipt',
+      'count' => $row['count'],
+    )
+  );
+}
+
+
+
+
+echo json_encode($response);
+
+mysqli_close($conn);
