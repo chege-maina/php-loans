27027b5afc9f011c00be728adda1c4d8a2fee171
Timothy Karani <kartimothy@gmail.com>
2021-04-17 16:10:57 +0300

    Ported expense mngt ui

--- payroll/expense_mngt.php
+++ payroll/expense_mngt.php
@@ -1,121 +1,92 @@
 <?php
-session_start();
-// If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
-  header('Location: ../index.php');
-  exit();
-}
+include "../includes/base_page/shared_top_tags.php"
 ?>
 
-<!DOCTYPE html>
-<html lang="en-US" dir="ltr">
+<form onsubmit="return submitForm();">
+  <style>
+    .hide-this {
+      display: none;
+    }
+  </style>
 
-<?php
-include_once '../includes/dbconnect.php';
-include '../includes/base_page/head.php';
-?>
+  <h5 class="p-2">Company Loans</h5>
 
-<main class="main" id="top">
-  <div class="container" data-layout="container">
-    <!--nav starts here -->
-    <?php
-    include '../includes/base_page/nav.php';
-    ?>
+  <div class="card">
+    <div class="card-body fs--1 p-4">
+      <!-- Content is to start here -->
+      <div class="row pb-2 ">
+        <div class="col">
+          <label for="exp_date" class="form-label">Date </label>
+          <input type="date" name="exp_date" id="exp_date" class="form-control" required>
+        </div>
+        <div class="col">
+          <label for="#" class="form-label">Select Employee </label>
+          <div class="input-group">
+            <input list="employee" name="employee_name" id="employee_name" class="form-select" required>
+            <datalist id="employee"></datalist>
+            <button type="button" class="btn btn-primary" onclick="selectEmployee();">Select</button>
+          </div>
+        </div>
+      </div>
+      <div class="row">
 
+        <div class="col">
+          <label for="design" class="form-label">Designation </label>
+          <input type="text" name="design" id="design" class="form-control" readonly>
+        </div>
+        <div class="col">
+          <label for="department" class="form-label">Department</label>
+          <input type="text" name="department" id="department" class="form-control" readonly>
+        </div>
+        <div class="col">
+          <label for="emp_no" class="form-label">Emp No#</label>
+          <input type="text" name="emp_no" id="emp_no" class="form-control" readonly>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col">
+          <label for="#" class="form-label">Loan Type</label>
+          <select class="form-select" id="loan_type" onchange="descriptionChanged()" required>
+            <option disabled selected value>--Select Loan Type--</option>
+            <option value="loan">Company Loan</option>
+            <option value="damage">Company Damage</option>
+          </select>
+        </div>
+        <div class="col">
+          <label for="#" class="form-label">Description</label>
 
-    <div class="content">
-      <?php
-      include '../navbar-shared.php';
-      ?>
-
-      <?php
-      include '../base_page/data_list_select.php';
-      ?>
-      <form onsubmit="return submitForm();">
-        <style>
-          .hide-this {
-            display: none;
-          }
-        </style>
-
-        <h5 class="p-2">Company Loans</h5>
-
-        <div class="card">
-          <div class="card-body fs--1 p-4">
-            <!-- Content is to start here -->
-            <div class="row pb-2 ">
-              <div class="col">
-                <label for="exp_date" class="form-label">Date </label>
-                <input type="date" name="exp_date" id="exp_date" class="form-control" required>
-              </div>
-              <div class="col">
-                <label for="#" class="form-label">Select Employee </label>
-                <div class="input-group">
-                  <input list="employee" name="employee_name" id="employee_name" class="form-select" required>
-                  <datalist id="employee"></datalist>
-                  <button type="button" class="btn btn-primary" onclick="selectEmployee();">Select</button>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-
-              <div class="col">
-                <label for="design" class="form-label">Designation </label>
-                <input type="text" name="design" id="design" class="form-control" readonly>
-              </div>
-              <div class="col">
-                <label for="department" class="form-label">Department</label>
-                <input type="text" name="department" id="department" class="form-control" readonly>
-              </div>
-              <div class="col">
-                <label for="emp_no" class="form-label">Emp No#</label>
-                <input type="text" name="emp_no" id="emp_no" class="form-control" readonly>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col">
-                <label for="#" class="form-label">Loan Type</label>
-                <select class="form-select" id="loan_type" onchange="descriptionChanged()" required>
-                  <option disabled selected value>--Select Loan Type--</option>
-                  <option value="loan">Company Loan</option>
-                  <option value="damage">Company Damage</option>
-                </select>
-              </div>
-              <div class="col">
-                <label for="#" class="form-label">Description</label>
-
-                <div id="desc1">
-                  <select class="form-select" id="desc_1">
-                    <option disabled selected value>--Select the description--</option>
-                    <option value="fees">School Fees</option>
-                    <option value="medical">Medical</option>
-                    <option value="develop">Development</option>
-                  </select>
-                </div>
-                <div id="desc2" class="hide-this">
-                  <select class="form-select" id="desc_2">
-                    <option disabled selected value>--Select the description--</option>
-                    <option value="fuel">Fuel</option>
-                    <option value="lost">Good Lost</option>
-                    <option value="workshop">Workshop</option>
-                    <option value="types">Types</option>
-                    <option value="breaks">Breakages</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col">
-                <label for="#" class="form-label">Initial Amount</label>
-                <div class="input-group">
-                  <span class="input-group-text is-static">KES</span>
-                  <input type="number" class="form-control" name="init_amt" id="init_amt" required>
-                  <div class="invalid-feedback">This field cannot be left blank.</div>
-                </div>
-              </div>
-              <!--
+          <div id="desc1">
+            <select class="form-select" id="desc_1">
+              <option disabled selected value>--Select the description--</option>
+              <option value="fees">School Fees</option>
+              <option value="medical">Medical</option>
+              <option value="develop">Development</option>
+            </select>
+          </div>
+          <div id="desc2" class="hide-this">
+            <select class="form-select" id="desc_2">
+              <option disabled selected value>--Select the description--</option>
+              <option value="fuel">Fuel</option>
+              <option value="lost">Good Lost</option>
+              <option value="workshop">Workshop</option>
+              <option value="types">Types</option>
+              <option value="breaks">Breakages</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col">
+          <label for="#" class="form-label">Initial Amount</label>
+          <div class="input-group">
+            <span class="input-group-text is-static">KES</span>
+            <input type="number" class="form-control" name="init_amt" id="init_amt" required>
+            <div class="invalid-feedback">This field cannot be left blank.</div>
+          </div>
+        </div>
+        <!--
               <div class="col">
                 <label for="#" class="form-label">Balance</label>
                 <div class="input-group">
@@ -125,66 +96,57 @@ include '../includes/base_page/head.php';
                 </div>
               </div>
               -->
-              <div class="col">
-                <label for="#" class="form-label">Installment Amount</label>
-                <div class="input-group">
-                  <span class="input-group-text is-static">KES</span>
-                  <input type="number" class="form-control" name="installment" id="installment" required>
-                  <div class="invalid-feedback">This field cannot be left blank.</div>
-                </div>
-              </div>
-              <div class="col">
-                <label for="#" class="form-label">Interest</label>
-                <div class="input-group">
-                  <span class="input-group-text is-static">%</span>
-                  <input type="number" class="form-control" name="interest" id="interest" required>
-                  <div class="invalid-feedback">This field cannot be left blank.</div>
-                </div>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col">
-                <label for="issue_date" class="form-label">Issued Date </label>
-                <input type="date" name="issue_date" id="issue_date" class="form-control" required>
-              </div>
-              <div class="col">
-                <label for="begin_date" class="form-label">Start Date </label>
-                <input type="date" name="begin_date" id="begin_date" class="form-control" required>
-              </div>
-              <div class="col">
-                <label for="#" class="form-label">Interest Type</label>
-                <select class="form-select" id="interest_type" required>
-                  <option disabled selected value>--Select Interest Type--</option>
-                  <option value="none">None</option>
-                  <option value="straight">Straight-Line</option>
-                  <option value="reducing">Reducing Balance</option>
-                </select>
-              </div>
-              <div class="col">
-                <label for="#" class="form-label">Fringe Benefit Tax</label>
-                <select class="form-select" id="fringe" required>
-                  <option disabled selected value>--Select Fringe Tax--</option>
-                  <option value="yes">Yes</option>
-                  <option value="no">No</option>
-                </select>
-              </div>
-            </div>
-            <button class="btn btn-falcon-primary btn-sm my-2" id="submit">
-              Submit
-            </button>
+        <div class="col">
+          <label for="#" class="form-label">Installment Amount</label>
+          <div class="input-group">
+            <span class="input-group-text is-static">KES</span>
+            <input type="number" class="form-control" name="installment" id="installment" required>
+            <div class="invalid-feedback">This field cannot be left blank.</div>
           </div>
         </div>
-      </form>
+        <div class="col">
+          <label for="#" class="form-label">Interest</label>
+          <div class="input-group">
+            <span class="input-group-text is-static">%</span>
+            <input type="number" class="form-control" name="interest" id="interest" required>
+            <div class="invalid-feedback">This field cannot be left blank.</div>
+          </div>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col">
+          <label for="issue_date" class="form-label">Issued Date </label>
+          <input type="date" name="issue_date" id="issue_date" class="form-control" required>
+        </div>
+        <div class="col">
+          <label for="begin_date" class="form-label">Start Date </label>
+          <input type="date" name="begin_date" id="begin_date" class="form-control" required>
+        </div>
+        <div class="col">
+          <label for="#" class="form-label">Interest Type</label>
+          <select class="form-select" id="interest_type" required>
+            <option disabled selected value>--Select Interest Type--</option>
+            <option value="none">None</option>
+            <option value="straight">Straight-Line</option>
+            <option value="reducing">Reducing Balance</option>
+          </select>
+        </div>
+        <div class="col">
+          <label for="#" class="form-label">Fringe Benefit Tax</label>
+          <select class="form-select" id="fringe" required>
+            <option disabled selected value>--Select Fringe Tax--</option>
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+        </div>
+      </div>
+      <button class="btn btn-falcon-primary btn-sm my-2" id="submit">
+        Submit
+      </button>
     </div>
-
-    <?php
-    include '../includes/base_page/footer.php';
-    ?>
   </div>
-  </body>
-
-</html>
+</form>
 
 <script>
   const employee = document.querySelector("#employee");
@@ -340,3 +302,7 @@ include '../includes/base_page/head.php';
 
   });
 </script>
+
+<?php
+include "../includes/base_page/shared_bottom_tags.php"
+?>
