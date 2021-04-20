a09f323d2a2eb3b929de5057f96dc7e673f39ae9
Chege Maina <mr.shei8@gmail.com>
2021-04-20 10:45:34 +0300

    Update load_loan_payment.php

--- includes/load_loan_payment.php
+++ includes/load_loan_payment.php
@@ -5,11 +5,11 @@ header("Content-type:application/json");
 include_once 'dbconnect.php';
 session_start();
 $bank = $_POST["bank"];
-$dis_date = $_POST["dis_date"];
+$loan_acc = $_POST["loan_acc"];
 $pay_date = $_POST["pay_date"];
 $pay_no = $_POST["pay_no"];
 
-$query = "SELECT * FROM tbl_loan_schedule WHERE bank = '$bank' and dis_date='$dis_date'and pay_date='$pay_date' and pay_no='$pay_no' and status='pending'";
+$query = "SELECT * FROM tbl_loan_schedule WHERE bank = '$bank' and loan_acc='$loan_acc'and pay_date='$pay_date' and pay_no='$pay_no' and status='pending'";
 
 $result = mysqli_query($conn, $query);
 $response = array();
@@ -17,11 +17,12 @@ $response = array();
 if ($row = mysqli_fetch_assoc($result)) {
     $bal = $row['balance'];
     $principle = $row['principle'];
+    $dis_date = $row['dis_date'];
     $bal = $principle + $bal;
     $interest = round($row['interest'], 2);
     $installment = round($row['installment'], 2);
 
-    $query2 = "SELECT * FROM tbl_loans WHERE bank_name = '$bank' and dis_date='$dis_date'and status='pending'";
+    $query2 = "SELECT * FROM tbl_loans WHERE bank_name = '$bank' and loan_acc='$loan_acc'and status='pending'";
     $result2 = mysqli_query($conn, $query2);
     if ($row2 = mysqli_fetch_assoc($result2)) {
         $loan_amt = $row2['amount'];
@@ -35,6 +36,7 @@ if ($row = mysqli_fetch_assoc($result)) {
             'balance' => $bal,
             'principle' => $principle,
             'interest' => $interest,
+            'dis_date' => $dis_date,
             'installment' => $installment,
             'charge_pc' => $charge_pc
         )
